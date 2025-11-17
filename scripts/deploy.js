6a15b8a7ee786f02d33c6b9d379e542c80fe8281
kiruxaspb <dvoreckovkirill@gmail.com>
2025-11-17 18:33:07 +0300

    updated deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,9 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account: ", deployer.address);
   
-    const BatchContract = await ethers.getContractFactory("BatchContract");
+    const BatchContract = await ethers.getContractFactory("TokenBatchTransfer");
   
-    const batchContract = await BatchContract.deploy();
+    const batchContract = await BatchContract.deploy("0xE47A39F774ABbB2b3B8b43088a966BBC0a6Af426");
 
     console.log("Contract deployed | Address: ", await batchContract.getAddress());
   }
